3b8d72b8633aa81088e03f7b603aa74a3ed66167
Yu Zhai <me@zhaiyusci.net>
2018-11-26 21:05:45 +0800

    mddvr: try solve multidimension problems

---
+++ mddvr.cc
@@ -0,0 +1,18 @@
+// This file is part of ydvr.
+//
+// Copyright (C) 2017 Yu Zhai <me@zhaiyusci.net>
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include"mddvr.h"
+
+using namespace Eigen;
+using namespace std;
+
+int sinc_podvr_md(const vector<double>& m, const vector<int>& N, const vector<int>& NPO, const vector<double>& a, const vecotr<double>& b, vector<VectorXd>& x, VectorXd& E, MatrixXd& wf, MatrixXd& H0_PODVR, const DoubleFunction1d& potential, bool perturbation){
+
+  return 0;
+}
+
