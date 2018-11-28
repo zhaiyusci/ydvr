0240a7b90e3b10640c6fae90506423ffd0825584
Yu Zhai <me@zhaiyusci.net>
2018-11-28 15:49:58 +0800

    frame of md podvr

--- mddvr.cc
+++ mddvr.cc
@@ -11,7 +11,25 @@
 using namespace Eigen;
 using namespace std;
 
-int sinc_podvr_md(const vector<double>& m, const vector<int>& N, const vector<int>& NPO, const vector<double>& a, const vecotr<double>& b, vector<VectorXd>& x, VectorXd& E, MatrixXd& wf, MatrixXd& H0_PODVR, const DoubleFunction1d& potential, bool perturbation){
+int sinc_podvr_md(const vector<double>& m, const vector<int>& N, const vector<int>& NPO, const vector<double>& a, const vecotr<double>& b, vector<VectorXd>& x, VectorXd& E, MatrixXd& wf, MatrixXd& H0_PODVR, const DoubleFunction1d& potential){
+  // Here m is the vector that stores effective masses of each orthogonal 
+  // vibrational modes, N the primitive basis set size, NPO PODVR grids number
+  // a, b the starting and ending point...
+  //
+  // Below three xxi's are for 1d problems.
+  vector<VectorXd> Ei(m.size());
+  vector<MatrixXd> wfi(m.size());
+  vector<MatrixXd> Hi_PODVR(m.size());
+  //
+  // Perform (size of m) 1d podvr.  
+  for(int i=0; i!=m.size(); ++i){
+    sinc_podvr_1d(m[i], N[i], NPO[i], a[i], double b[i], x[i], Ei[i], 
+        wfi[i], MatrixXd& Hi_PODVR, potential);
+  }
+
+  // Construct the md hamiltonian on PODVR grids
+
+
 
   return 0;
 }
