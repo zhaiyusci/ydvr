56297e3e2930339c8c7150e5abeac2a40213c55a
Yu Zhai <me@zhaiyusci.net>
2020-07-19 20:17:54 +0800

    move tests to src/tests

--- src/tests/harmonic_test.cc
+++ src/tests/harmonic_test.cc
@@ -7,13 +7,12 @@
 // with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include"ydvr.h"
-#include"co_pes_2016/co_pes_2016.h"
 
 using namespace std;
 using namespace yDVR;
 int main(){
   Log::set("harmonic_test.out");
-  Oscillator codvr(6.856208638000723*1822.888486209, [](double r){return copes(r/1.8897261254578281)/219474.6313702;});
+  Oscillator codvr(1.0, [](double r){return 0.5*r*r;});
   /*
   for (double i=0.1; i<=5; i+=0.02){
     // cout << i << "\t"<<  co.potential(i)<< endl;
@@ -24,7 +23,7 @@ int main(){
 
 
   std::vector<Scalar> force_c;
-  double re = 1.128322531930*1.8897261254578281;
+  double re = 0.;
   double h = 1e-3;
   double f_2=codvr.potential(re-2*h);
   double f_1=codvr.potential(re-1*h);
@@ -36,15 +35,15 @@ int main(){
   force_c.push_back(1./h/h*(f1+f_1-2*f0));                // 2nd
   force_c.push_back(0.5/h/h/h*(f2-2*f1-f_2+2*f_1));       // 3rd
   force_c.push_back(1./h/h/h/h*(f2+f_2-4*f1-4*f_1+6*f0)); // 4th
-  OscillatorTaylor co(6.856208638000723*1822.888486209, re, force_c);
+  OscillatorTaylor co(1., re, force_c);
   // OscillatorTaylor co(1., 0, {0, 0, 1});
   HarmonicRepresentation hr(co,10 );
   // SincDVR sincdvr(co,1.33, 4.33,100 );
-  SincDVR sincdvr(codvr,1.33, 4.33,100 );
+  SincDVR sincdvr(codvr,-10.,10.,200 );
     cout << "i     sincdvr          hr" << endl;
   for (int i=0; i!=5; ++i){
     // cout << i << "\t" << sincdvr.energyLevel(i) << "\t" << hr.energyLevel(i) << endl;
-    cout << i << "\t" << (sincdvr.energyLevel(i))*219474.6313702 << "\t" << hr.energyLevel(i)*219474.6313702 << endl;
+    cout << i << "\t" << (sincdvr.energyLevel(i))<< "\t" << hr.energyLevel(i)<< endl;
   }
   PODVR po(sincdvr,5);
   cout << po.grids() << endl;
--- src/tests/sincdvr_test.cc
+++ src/tests/sincdvr_test.cc
@@ -7,16 +7,16 @@
 // with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #include"ydvr.h"
-#include"co_pes_2016/co_pes_2016.h"
 
 using namespace std;
 using namespace yDVR;
 int main(){
   Log::set("sincdvr_test.out");
-  Oscillator co(6.856208638000723*1822.888486209, [](double r){return copes(r/1.8897261254578281)/219474.6313702;});
-  SincDVR sincdvr(co,1.33,4.3,100 );
+  Oscillator harm(1., [](double r){return 0.5*r*r;});
+  SincDVR sincdvr(harm,-10.,10.,200 );
+  PODVR podvr(sincdvr, 5);
   for (int i=0; i!=5; ++i){
-    cout << i << "\t"<< sincdvr.energyLevel(i)*219474.6313702 << endl;
+    cout << i << "\t"<< sincdvr.energyLevel(i) <<"   " << podvr.energyLevel(i)<< "   " << i*1.+0.5 << endl;
   }
   return 0;
 }
